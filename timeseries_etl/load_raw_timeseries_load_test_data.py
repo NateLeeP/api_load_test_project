01735224f7c16e4643601f54fb2c378496eece70
Nate P <nlpruitt@gmail.com>
2023-07-29 17:47:27 -0500

    time series data etl folder structure

---
+++ timeseries_etl/load_raw_timeseries_load_test_data.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+def load_loadtest_timeseries_from_csv(path_to_csv: str):
+    """
+    Return pandas DF with metrics http_req_duration, http_req_failed, http_reqs (iteration)
+    
+    Arguments: String with path to CSV
+
+    Columns: http_req_duration, time. 
+
+    """
+    raw_timeseries_df = pd.read_csv(path_to_csv)
+    # Filter out by metric values
+    return raw_timeseries_df
+
+def main():
+    df = load_loadtest_timeseries_from_csv('/home/npruitt/repos/csv_files/express_app_load_test_timeseries_data.csv')
+    print(df.head())
+
+if __name__ == "__main__":
+    main()
